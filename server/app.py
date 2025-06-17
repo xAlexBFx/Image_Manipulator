056e8d1f330f5879b86bd0eb54c248d57d58cb9c
Alex Betances <alexbetancesf@gmail.com>
2025-06-16 20:02:06 -0400

    ...

--- server/app.py
+++ server/app.py
@@ -296,5 +296,4 @@ def process_image():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8080)
